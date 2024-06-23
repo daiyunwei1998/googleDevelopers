519b841839f4b083f2330993167cf53b26557bce
Yunwei Dai <daiyunwei1998@gmail.com>
2024-06-23 15:30:41 +0800

    done with the contents

--- components/WhatNew/WhatNew.js
+++ components/WhatNew/WhatNew.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import ButtonSquared from '../ButtonSquared/ButtonSquared'
 import styles from "./WhatNew.module.css";
+import Image from 'next/image';
 
 export default function WhatNew() {
   return (
     <section className = {styles.section_what_new}>
         <div className = {styles.container}>
-            <img className = {styles.img_what_new} src = "/img/android-studio-jellyfish_480.png" />
+            <Image 
+            className = {styles.img_what_new}
+            width={0}
+            height={0}
+            sizes="100vw"
+            src = "/img/android-studio-jellyfish_480.png" />
             <div className = {styles.div_what_new}>
                 <p>WHAT'S NEW IN ANDROID</p>
-                <h1>Android Studio Jellyfish is now stable</h1>
+                <h1 className = {styles.h1}>Jellyfish is now stable</h1>
                 <p className = {styles.p}>Android Studio Jellyfish is here! Dive into cutting-edge AI features like Gemini in Android Studio and seamless services integrations like Android Device Streaming.</p>
-                <ButtonSquared url = "https://developer.android.com/studio" text = "Download now"  />
+                <a href = "https://developer.android.com/studio">
+                  <button className = {styles.button}><b>Download now</b></button>
+                </a>    
             </div>
         </div>
 
